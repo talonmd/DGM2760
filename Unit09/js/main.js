92841c19322d7fd60c6c807d12d3be4fc79fc203
Talon Durrant <talonmdurrant@gmail.com>
2020-04-10 17:42:53 -0600

    unit 9 mostly complete

---
+++ Unit09/js/main.js
@@ -0,0 +1,21 @@
+function duplicateMenu() {
+  // pulling all a elements from primary navigation
+  let topList = document.querySelectorAll("ul#primaryNavigation li a");
+
+  // mobile navigation list
+  let newList = document.createElement("ul");
+
+  topList.forEach((menuItem) => {
+    let newLI = document.createElement("li");
+    let newLink = document.createElement("a");
+
+    newLink.textContent = menuItem.textContent;
+    newLink.setAttribute("href", menuItem.getAttribute("href"));
+
+    document.querySelector("#smallNavArea").appendChild(newList);
+    newList.appendChild(newLI);
+    newLI.appendChild(newLink);
+  });
+}
+
+duplicateMenu();
